f13360e61a5cec12bbd54460f3b6603567cd339a
goku <389651824@qq.com>
2022-09-08 01:21:27 +0800

    contact us页面间距问题

--- src/views/a-bottle-salt/Contacts.vue
+++ src/views/a-bottle-salt/Contacts.vue
@@ -13,34 +13,34 @@
 
     <img style="top: 17.37rem;width: 78.06rem;left: 6rem;height: 14.12rem;position: absolute" :src="secondImg"/>
 
-    <div style="width: 100%;height: 70%;position: relative">
+<!--    <div style="width: 100%;height: 70%;position: relative">-->
 
-    </div>
+<!--    </div>-->
 
-    <div style="width: 100%;height: 7.142857142857143em;padding: 0 6rem">
+    <div style="width: 78em;height: 5.75em;padding: 38.43em 5.8rem ">
       <table class="divBase" style="font-weight: 500">
         <tr>
-          <td style="width: 33%;height: 100%;">
-            <div class="BoldItalic" style="width: 21em;height: 1.2857142857142858em;color: #E3E1DB;font-size: 0.875rem;margin-bottom: 1.4285714285714286em">
-              PHONE NUMBER：
-              <span style="font-size: 0.875rem;color: #71706E">{{contacts.tel}}</span>
+          <td style="width: 18.375em;height: 100%;">
+            <div class="BoldItalic" style="width: 18.375em;height: 1.2857142857142858em;color: #E3E1DB;font-size: 14px;letter-spacing: 0.05em;margin-bottom: 1.35em;line-height: 18px">
+              PHONE NUMBER:
+              <span style="font-size: 14px;color: #71706E">{{contacts.tel}}</span>
             </div>
-            <div class="BoldItalic" style="width: 21em;height: 6.957142857142857em;color: #71706E!important;font-size: 0.875rem;">
+            <div class="BoldItalic" style="width: 18.375em;height: 6.957142857142857em;color: #71706E!important;font-size: 0.875rem;line-height: 18px;letter-spacing: 0.55px;">
               {{contacts['address'+$store.getters.getLanguage]}}
             </div>
           </td>
-          <td style="width: 33%;height: 100%;padding-left: 9%">
-            <div class="BoldItalic" style="width: 21em;height: 1.2857142857142858em;color: #E3E1DB;font-size: 0.875rem;margin-bottom: 1.4285714285714286em">
+          <td style="width: 18.375em;height: 100%;padding-left: 15.732142857142858em">
+            <div class="BoldItalic" style="width: 21em;height: 1.2857142857142858em;color: #E3E1DB;font-size: 0.875rem;margin-bottom: 1.3em;letter-spacing: 0.05em">
               OUR SOCIAL：
             </div>
-            <div style="width: 100%;height: 70%;color: #71706E;font-size: 0.875rem;font-style: italic;">
-                <div v-for="(item,index) in contactImages" style="width: 2rem;height: 20%;position: relative;display: inline-block;" :style="index != 0 ? 'margin-left: 0.75rem':''">
-                  <a :href="item.contactUrl" target="_blank"><img style="position: absolute;width: 1.7142857142857142em;height: 1.7142857142857142em" :src="item.imageUrl"/></a>
+            <div style="width: 19em;height: 70%;color: #71706E;font-size: 0.875rem;font-style: italic;">
+                <div v-for="(item,index) in contactImages" style="width: 1.7142857142857142em;height: 1.7142857142857142em;margin-right: 0.35em;position: relative;display: inline-block;" :style="index != 0 ? 'margin-left: 0.75rem':''">
+                  <a :href="item.contactUrl" target="_blank"><img style="position: absolute;width: 1.7142857142857142em;height: 1.7142857142857142em;" :src="item.imageUrl"/></a>
                 </div>
             </div>
           </td>
-          <td style="width: 33%;height: 100%;padding-left: 15%">
-            <div class="BoldItalic" style="width: 21em;height: 1.2857142857142858em;color: #E3E1DB;font-size: 0.875rem;margin-bottom: 1.4285714285714286em">
+          <td style="width: 33%;height: 100%;padding-left: 17.2em;letter-spacing: 0.05em">
+            <div class="BoldItalic" style="width: 21em;height: 1.2857142857142858em;color: #E3E1DB;font-size: 0.875rem;margin-bottom: 1.3em">
               E-MAIL：
             </div>
             <div class="BoldItalic" style="width: 11.571428571428571em;height: 70%;color: #71706E;font-size: 0.875rem">
