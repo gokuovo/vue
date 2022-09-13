4a5440b92c14f2b4cc2c7e6dceb8e4df5eed6f78
tumingjie <5549590@qq.com>
2022-09-13 15:28:10 +0800

    最终版5.0

--- src/views/a-bottle-salt/News.vue
+++ src/views/a-bottle-salt/News.vue
@@ -1,7 +1,7 @@
 <template>
   <div ref="alignCenter" class="alignCenter" style="width: 90rem;height: 100%">
     <div ref="backGroundBox" class="box">
-      <div class="image-wrap" style="opacity: 0.01">
+      <div class="image-wrap">
         <div class="image" :style="{backgroundImage: `url(${firstImg})`}"></div>
       </div>
     </div>
--- src/views/a-bottle-salt/Partners.vue
+++ src/views/a-bottle-salt/Partners.vue
@@ -1,7 +1,7 @@
 <template>
   <div ref="alignCenter" class="alignCenter" style="width: 90rem;height: 100%">
     <div ref="backGroundBox" class="box">
-      <div class="image-wrap" style="opacity: 0.05">
+      <div class="image-wrap">
         <div class="image" :style="{backgroundImage: `url(${firstImg})`}"></div>
       </div>
     </div>
@@ -14,13 +14,24 @@
       <table class="divBase">
         <tr>
           <td style="width: 80%;height: 100%;vertical-align: top;padding-top: 6.06rem">
-            <div class="divBase selfDefineScroll" style="height: 70%;width: 72.81rem;overflow-y: auto;margin-left: 6rem;margin-bottom: 10rem">
-              <div v-for="(item,index) in partners" class="divClass" :style="{marginLeft: `${index % 5 != 0 ? '1.5rem' : ''}`}">
-                <a :href="item.partnerLink" :target="item.partnerLink.indexOf('http') != -1 ? '_blank' : '_self' ">
-                  <img class="linkHover"  style="position: absolute;width: 11.75rem;height: 11.75rem;object-fit: cover;" :src="item.partnerUrl" />
-                </a>
-              </div>
+<!--            <div class="divBase selfDefineScroll" style="height: 70%;width: 72.81rem;overflow-y: auto;margin-left: 6rem;margin-bottom: 10rem">-->
+<!--              <div v-for="(item,index) in partners" class="divClass" :style="{marginLeft: `${index % 5 != 0 ? '1.5rem' : ''}`}">-->
+<!--                <a :href="item.partnerLink" :target="item.partnerLink.indexOf('http') != -1 ? '_blank' : '_self' ">-->
+<!--                  <img class="linkHover"  style="position: absolute;width: 11.75rem;height: 11.75rem;object-fit: cover;" :src="item.partnerUrl" />-->
+<!--                </a>-->
+<!--              </div>-->
+<!--            </div>-->
+
+            <div style="height: 32rem;width: 72.81rem;margin-left: 6rem;margin-bottom: 10rem">
+              <vue-custom-scrollbar class="scroll-area" :settings="settings" style="height: 32rem;width: 72.81rem;" @ps-scroll-y="endY">
+                <div v-for="(item,index) in partners" class="divClass" :style="{marginLeft: `${index % 5 !== 0 ? '1.5rem' : ''}`}">
+                  <a :href="item.partnerLink" :target="item.partnerLink.indexOf('http') !== -1 ? '_blank' : '_self' ">
+                    <img class="linkHover"  style="position: absolute;width: 11.75rem;height: 11.75rem;object-fit: cover;" :src="item.partnerUrl" />
+                  </a>
+                </div>
+              </vue-custom-scrollbar>
             </div>
+
           </td>
           <td style="width: 20%;">
             <contacts></contacts>
@@ -35,14 +46,22 @@
   import toolbar from './components/toolbar'
   import contacts from './components/contacts'
   import boxes from './commonScripts/moveBackground'
+  import vueCustomScrollbar from 'vue-custom-scrollbar'
+  import "vue-custom-scrollbar/dist/vueScrollbar.css"
 
   import {getAllPartners,getContactImage_back} from './requestScript/Partners'
 
   export default {
     name: 'Partners',
-    components: { toolbar,contacts },
+    components: { toolbar,contacts,vueCustomScrollbar },
     data(){
       return {
+        settings: {
+          suppressScrollY: false,
+          suppressScrollX: true,
+          wheelPropagation: false,
+          maxScrollbarLength:50,
+        },
         firstImg: '',
         partners:[
         ]
@@ -84,11 +103,50 @@
         size: boxElement.offsetWidth
       })
     },
+    methods:{
+      endY(evt){
+        console.log(evt);
+      }
+    }
   }
 </script>
 
 <style scoped>
   @import './commonCSS/moveBackground.scss';
+
+  /*新滚动条样式*/
+  >>> .ps--active-y>.ps__rail-y{
+    background-color: #383838!important;
+  }
+
+  >>> .ps__rail-y{
+    width: 3px;
+    opacity: 1;
+  }
+
+   >>> .ps .ps__rail-y:focus, >>> .ps .ps__rail-y:hover{
+    background-color: #383838;
+    opacity: 1;
+  }
+
+   >>> .ps:hover>.ps__rail-y{
+     opacity: 1;
+   }
+
+  >>> .ps__thumb-y {
+    background: #d1d8e6;
+    border-radius: 19px;
+    background-clip: content-box;
+    position: absolute;
+    width: 3px;
+    right: 0;
+  }
+
+  >>> .ps__rail-y.ps--clicking .ps__thumb-y, >>> .ps__rail-y:focus>.ps__thumb-y, >>> .ps__rail-y:hover>.ps__thumb-y {
+    background-color: #d1d8e6;
+    width: 3px;
+  }
+  /*新滚动条样式*/
 
   .divClass{
     display: inline-block;
@@ -118,6 +176,7 @@
     background-color: rgba(0, 0, 0, 0.7);
   }
 
+  /*老滚动条样式*/
   .selfDefineScroll::-webkit-scrollbar {
     width: 3px;
     background-color: #b5b1b1;
@@ -135,6 +194,7 @@
     border-radius: 19px;
     background-clip: content-box;
   }
+  /*老滚动条样式*/
 
 
   >>> .el-breadcrumb__inner{
